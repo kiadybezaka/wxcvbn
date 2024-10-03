96201ec162ffe81f676f200fc244effe7db31f23
Kiady Razafindrabe <kiady.razafindrabe@gmail.com>
2024-10-03 09:57:22 +0200

    [sp1] add link on readme

--- README.md
+++ README.md
@@ -22,3 +22,13 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Deploy on gh-pages
+```
+https://learnvue.co/articles/deploy-vue-to-github-pages
+```
+
+### Project link
+```
+https://kiadybezaka.github.io/wxcvbn/
+```
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "https://kiadybezaka.github.io/wxcvbn/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/wxcvbn/" : "/",
 })
